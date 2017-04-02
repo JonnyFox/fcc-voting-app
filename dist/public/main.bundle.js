1acc8bdd5aa6bf5ed8c7fb01253f97d18d334d8c
jfox <fox.jonny@gmail.com>
2017-04-02 15:37:51 +0200

    Resolving dependencies

--- dist/public/main.bundle.js
+++ dist/public/main.bundle.js
@@ -777,7 +777,7 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var PollService = (function (_super) {
     __extends(PollService, _super);
     function PollService(http) {
-        var _this = _super.call(this, http, 'http://localhost:8999/api/polls') || this;
+        var _this = _super.call(this, http, 'http://localhost/api/polls') || this;
         _this.http = http;
         return _this;
     }
@@ -831,13 +831,8 @@ var _a;
 
 "use strict";
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return environment; });
-// The file contents for the current environment will overwrite these during build.
-// The build system defaults to the dev environment which uses `environment.ts`, but if you do
-// `ng build --env=prod` then `environment.prod.ts` will be used instead.
-// The list of which env maps to which file can be found in `angular-cli.json`.
-// The file contents for the current environment will overwrite these during build.
 var environment = {
-    production: false
+    production: true
 };
 //# sourceMappingURL=environment.js.map
 
